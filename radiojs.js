0f7fbd50e94b680da3b5aa1b0c1676088c59f667
priyash555 <31501889+priyash555@users.noreply.github.com>
2019-01-21 13:10:28 +0530

    Add files via upload

---
+++ radiojs.js
@@ -0,0 +1,96 @@
+// function revealAboutUs()
+// {
+// 	document.getElementById("AboutUs").style.display = 'block';
+// 	document.getElementById("newsfeed").style.display = 'none';
+// 	document.getElementById("contact").style.display = 'none';
+// 	document.getElementById("radioIIITM").style.display = 'none';
+// 	document.getElementById("foot").style.position = '';
+// 	document.getElementById("foot").style.width = '100%';
+// 	document.getElementById("abt").style.backgroundColor = 'black';
+// 	document.getElementById("abt").style.borderRadius = '5px';
+// 	document.getElementById("contact1").style.backgroundColor = 'transparent';
+// 	document.getElementById("contact1").style.borderRadius = '0px';
+// 	document.getElementById("home").style.backgroundColor = 'transparent';
+// 	document.getElementById("home").style.borderRadius = '0px';
+// 	document.getElementById("fm").style.backgroundColor = 'transparent';
+// 	document.getElementById("fm").style.borderRadius = '0px';
+// 	limit=100;
+// }
+// function revealContactUs()
+// {
+// 	document.getElementById("AboutUs").style.display = 'none';
+// 	document.getElementById("newsfeed").style.display = 'none';
+// 	document.getElementById("contact").style.display = 'block';
+// 	document.getElementById("radioIIITM").style.display = 'none';
+// 	document.getElementById("foot").style.position = 'fixed';
+// 	document.getElementById("foot").style.width = '100%';
+// 	document.getElementById("contact1").style.backgroundColor = 'black';
+// 	document.getElementById("contact1").style.borderRadius = '5px';
+// 	document.getElementById("abt").style.backgroundColor = 'transparent';
+// 	document.getElementById("abt").style.borderRadius = '0px';
+// 	document.getElementById("home").style.backgroundColor = 'transparent';
+// 	document.getElementById("home").style.borderRadius = '0px';
+// 	document.getElementById("fm").style.backgroundColor = 'transparent';
+// 	document.getElementById("fm").style.borderRadius = '0px';
+// }
+
+// function revealHome() 
+// {
+// 	document.getElementById("AboutUs").style.display = 'none';
+// 	document.getElementById("newsfeed").style.display = 'block';
+// 	document.getElementById("contact").style.display = 'none';
+// 	document.getElementById("radioIIITM").style.display = 'none';
+// 	document.getElementById("foot").style.position = '';
+// 	document.getElementById("foot").style.width = '100%';
+// 	document.getElementById("home").style.backgroundColor = 'black';
+// 	document.getElementById("home").style.borderRadius = '5px';
+// 	document.getElementById("abt").style.backgroundColor = 'transparent';
+// 	document.getElementById("abt").style.borderRadius = '0px';
+// 	document.getElementById("contact1").style.backgroundColor = 'transparent';
+// 	document.getElementById("contact1").style.borderRadius = '0px';
+// 	document.getElementById("fm").style.backgroundColor = 'transparent';
+// 	document.getElementById("fm").style.borderRadius = '0px';
+// 	limit=500;
+// }
+
+// function revealRadio() 
+// {
+// 	document.getElementById("AboutUs").style.display = 'none';
+// 	document.getElementById("newsfeed").style.display = 'none';
+// 	document.getElementById("contact").style.display = 'none';
+// 	document.getElementById("radioIIITM").style.display = 'block';
+// 	document.getElementById("foot").style.position = 'fixed';
+// 	document.getElementById("foot").style.width = '100%';
+// 	document.getElementById("fm").style.backgroundColor = 'black';
+// 	document.getElementById("fm").style.borderRadius = '5px';
+// 	document.getElementById("abt").style.backgroundColor = 'transparent';
+// 	document.getElementById("abt").style.borderRadius = '0px';
+// 	document.getElementById("contact1").style.backgroundColor = 'transparent';
+// 	document.getElementById("contact1").style.borderRadius = '0px';
+// 	document.getElementById("home").style.backgroundColor = 'transparent';
+// 	document.getElementById("home").style.borderRadius = '0px';
+// }
+
+
+// $(function() {
+// 	$('#cl').on('click', function() {
+// 		$('#AboutUs').addClass('animated fadeInUp fast');
+// 		$('#kli').addClass('animated fadeInUp fast');
+// 		$('#newsfeed').addClass('animated fadeInUp fast');
+// 	});
+// 	$('#cli').on('click', function() {
+// 		$('#AboutUs').addClass('animated fadeInUp fast');
+// 		$('#kli').addClass('animated fadeInUp fast');
+// 		$('#newsfeed').addClass('animated fadeInUp fast');
+// 	});
+// 	$('#clj').on('click', function() {
+// 		$('#AboutUs').addClass('animated fadeInUp fast');
+// 		$('#kli').addClass('animated fadeInUp fast');
+// 		$('#newsfeed').addClass('animated fadeInUp fast');
+// 	});
+// 	$('#clk').on('click', function() {
+// 		$('#AboutUs').addClass('animated fadeInUp fast');
+// 		$('#kli').addClass('animated fadeInUp fast');
+// 		$('#newsfeed').addClass('animated fadeInUp fast');
+// 	});
+// });
